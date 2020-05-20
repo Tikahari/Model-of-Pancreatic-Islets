65c3a6b5dfaf928412d7a83d8333d4405a3931d3
Tikahari Khanal <tikaharikhanal@login4.ufhpc>
2020-05-20 11:02:47 -0400

    piecewise, varying cell size in orient.py

--- Build/Orientation/orient.py
+++ Build/Orientation/orient.py
@@ -7,52 +7,84 @@ import random
 import math
 import sys
 
+center_prob = 1
 # cell sizes
 sizes = {'A': 10, 'B': 10, 'D': 10}
 # dimension of islets (will determine size of box that will enclose islet)
-n = 7
+n = 10
 # default probability of cell (60% chance of b-cell, 30% of a-cell, 10% of d-cell)
-ct = [0.6, 0.85, 1]
-# scaling factor (percentage of total volume the islet will occupy)
-sc = 0.7
+probabilities = [0.15, 0.75]
 
 # wrapper class to store type and dimensions of cell
 class Cell:
     def __init__(self, typ):
         # within 10% of desired size
         if(typ is None):
-            self.d = -1
+            self.diam = -1
             self.typ = "Z"
         self.typ = typ
         # diameter within 10% of given
         self.diam = random.randint(math.floor(sizes[self.typ]-sizes[self.typ]*random.random()/10), math.ceil(sizes[self.typ]+sizes[self.typ]*random.random()*10))
 # wrapper class to store probability a point is alpha/beta/delta
 class Probability:
-    def __init__(self, a, b, d):
-        if a is None or b is None or d is None:
-            self.a = -1
-            self.b = - 1
-            self.d = -1
-            return
-        self.a = a
-        self.b = b
-        self.d = d
+    def __init__(self, a, b, radius):
+        # beta cell probability
+        self.b = b - a
+        # ratio of delta to alpha cells
+        d = 1 - b
+        self.ratio = d / a
+        self.radius = radius
+        self.getDescent(center_prob)
+    # m (y=mx+b => m = -2*b/(x^2) from 0 to 1/3rd radius where b is 85 (85% probability of beta cell in center))
+    # interval = -(b-c)+-sqrt((b-c)^2-4*(m/2)*(c*r-r*p(beta)))/m
+    # average probability of cell type must be as defined in cell_type (calculate sum of piecewise from 1/3rd radius out)
+    def getDescent(self, c_p):
+        # beta cell has constant probability of 0.4 from 1/3rd out
+        # self.m = (self.b*self.radius - 0.4 * (2*self.radius/3) - c_p * (self.radius/3)) * 2 / ((self.radius/3)**2) 
+        self.m = 0
+    def getProbability(self, x, y, z):
+        ret = []
+        dist = math.sqrt((self.radius-x)**2 + (self.radius-y)**2 + (self.radius-z)**2)
+        print('dist', dist)
+        # piecewise
+        if(dist > self.radius / 3):
+            # alpha = (1 - self.b)/ (1 + self.ratio)
+            # delta = 1- self.b - alpha
+            alpha = (1 - 0)/ (1 + self.ratio)
+            delta = 1- 0 - alpha
+            print('m', self.m, 'ratio', self.ratio, 'radius', self.radius)
+            # print((1-self.b)/(1+self.ratio), self.b, self.ratio*(1-self.b)/(1+self.ratio), delta + alpha + self.b)
+            print((1-0)/(1+self.ratio), 0, self.ratio*(1-0)/(1+self.ratio), delta + alpha + 0)
+            # return [alpha, self.b + alpha]
+            return [alpha, alpha]
+        # calculate probabilities of alpha/beta/delta in this position
+        beta = self.m * dist + center_prob
+        print(self.m, dist, self.m*dist, center_prob)
+        alpha = (1 - beta)/ (1 + self.ratio)
+        delta = 1 - beta - alpha
+        print('m', self.m, 'ratio', self.ratio, 'radius', self.radius)
+        print('descent', alpha, beta + alpha, delta, delta + alpha + beta)
+        return [alpha, beta + alpha]
+
 # 3D matrix and accompanying operations that represent the spatial relationship between cells
 class Space:
-    def __init__(self, cell_type, dimen, sc, f):
+    def __init__(self, cell_p, cell_s, dimen, f):
         # file where data will be written to
         self.f = f
-        # scaling factor
-        self.sc = sc
         # probability alpha/beta/delta
-        self.cell_type = cell_type
+        self.cell_type = cell_p
+        self.cell_s = cell_s
         self.d = dimen
         self.setup()
     def setup(self):
-        # exponential distribution for beta cells (uniform for alpha, delta)
-        self.b_dist = expon()
+        # distribution for beta cells, 
+        # relative abundance according to cell_type
+        # piecewise, linear decrease in beta cell probability from center to 1/3rd radius, constant probability after
+        self.dist = Probability(self.cell_type[0], self.cell_type[1], self.d/2)
         # position of cells
         self.cpos = []
+        # size of cells
+        self.csize = []
         print('setting cells')
         # 3d matrix of cells
         self.cs = [[[[None] for i in range(self.d)] for j in range(self.d)] for k in range(self.d)]
@@ -61,59 +93,71 @@ class Space:
         self.ps = [[[[None] for i in range(self.d)] for j in range(self.d)] for k in range(self.d)]
         self.setProbabilities()
     def setProbabilities(self):
+        a = 0 
         for i in range(self.d):
             for j in range(self.d):
                 for k in range(self.d):
-                    self.ps[i][j][k] = Probability(self.cell_type[0], self.cell_type[1], self.cell_type[2])
+                    a += 1
+                    print(a, sep=' ')
+                    self.ps[i][j][k] = self.dist.getProbability(i,j,k)
     # fill islet randomly
     def mainLoop_A(self):
         c = 0
         while c < self.d:
-            typ = getCell(random.random())
+            typ = self.getCell(random.random())
             # x/y/z coordinates
             pos = random.sample(range(0, self.d), 3)
             # check if cell exists
             if(self.cs[pos[0]][pos[1]][pos[2]] == None):
                 continue
             # if position is vacant, add cell to space
-            self.cs[pos[0]][pos[1]][pos[2]] = Cell(typ)
+            cell = Cell(typ)
+            self.cs[pos[0]][pos[1]][pos[2]] = cell
             # store type and position
             self.cpos.append([typ, pos[0], pos[1], pos[2]])
+            # store cell size
+            self.csize.append(cell.diam)
             c += 1
         return
     # fill islet radially
     def mainLoop_B(self):
-        radius = self.d /4
-        i = math.floor(self.d/2 - radius)
-        j = math.floor(self.d/2 - radius)
-        k = math.floor(self.d/2 - radius)
+        # radius = self.d /4
+        i = 0
+        j = 0
+        k = 0
         c = 0
-        while i < self.d/2 + radius:
-            while j < self.d/2 + radius:
-                while k < self.d/2 + radius:
-                    if(self.cs[i][j][k] == None or math.sqrt((self.d/2-i)**2+(self.d/2-j)**2+(self.d/2-k)**2) > radius):
+        while i < self.d:
+            while j < self.d:
+                while k < self.d:
+                    if(self.cs[i][j][k] == None or math.sqrt((self.d/2-i)**2+(self.d/2-j)**2+(self.d/2-k)**2) > self.d/2):
                         k += 1
                         continue
-                    typ = getCell(random.random())
+                    typ = self.getCell(random.random(), i, j, k)
                     # if position is vacant, add cell to space
-                    self.cs[i][j][k] = Cell(typ)
+                    cell = Cell(typ)
+                    self.cs[i][j][k] = cell
                     # store type and position
                     self.cpos.append([typ, i, j, k])
+                    self.csize.append(cell.diam)
                     c += 1
                     k += 1
                 j += 1
-                k = math.floor(self.d/2 - radius)
+                k = 0
+                # k = math.floor(self.d/2 - radius)
             i += 1
-            j = math.floor(self.d//2 - radius)
+            j = 0
+            # j = math.floor(self.d//2 - radius)
     def plot(self):
         print('plotting')
         x = {'A': [], 'B': [], 'D': []}
         y = {'A': [], 'B': [], 'D': []}
         z = {'A': [], 'B': [], 'D': []}
+        s = {'A': [], 'B': [], 'D': []}
         for i in self.cpos:
             x[i[0]].append(i[1])
             y[i[0]].append(i[2])
             z[i[0]].append(i[3])
+            s[i[0]].append(self.cs[i[1]][i[2]][i[3]].diam)
         fig = plt.figure()
         a = mpatches.Patch(color='red', label='Alpha')
         b = mpatches.Patch(color='blue', label='Beta')
@@ -121,25 +165,25 @@ class Space:
         ax = fig.add_subplot(111, projection='3d')
         ax.legend(handles=[a,b,d])
         ax.set_title('Spatial Representation of Islet')
-        ax.axis('off')
-        ax.scatter(x['A'], y['A'], z['A'], c='red')
-        ax.scatter(x['B'], y['B'], z['B'], c='blue')
-        ax.scatter(x['D'], y['D'], z['D'], c='green')
+        # ax.axis('off')
+        ax.scatter(x['A'], y['A'], z['A'], c='red', s=s['A'])
+        ax.scatter(x['B'], y['B'], z['B'], c='blue', s=s['B'])
+        ax.scatter(x['D'], y['D'], z['D'], c='green', s=s['D'])
         plt.savefig('distributions/'+sys.argv[2])
         plt.show()
     def writeData(self):
         prob = ''
-        c = 0
-        cells = ['Probability_Alpha', 'Probability_Beta', 'Probability_Delta']
+        siz = ''
+        c = [0, ['A', 'B', 'D']]
+        cells_p = ['Probability_Alpha', 'Probability_Beta', 'Probability_Delta']
+        cells_s = ['Size_Alpha', 'Size_Beta', 'Size_Delta']
         for i in range(len(self.cell_type)):
-            x = self.cell_type[i]*100 - c
-            c += x
-            prob += cells[i] + ' = ' + str(x) + '\n'
-        print(prob)
+            x = self.cell_type[i]*100 - c[0]
+            c[0] += x
+            prob += cells_p[i] + ' = ' + str(x) + '\n'
+            siz += cells_s[i] + ' = ' + str(self.cell_s[c[1][i]]) + '\n'
         config = '[Config]\n'+\
-        'dimensions='+str(self.d)+'\n'+\
-        'scaling_factor='+str(self.sc)+'\n'+\
-        prob
+        'dimensions='+str(self.d)+'\n'+prob+siz
         data = '[Data]\n'
         for i in self.cpos:
             data += str(i[0])+' '+str(i[1])+' '+str(i[2])+' '+str(i[3])+'\n'
@@ -147,21 +191,18 @@ class Space:
         file.write(config)
         file.write(data)
         file.close()
-def getCell(r):
-    if r <= ct[0]:
-        return 'A'
-    elif r <= ct[1]:
-        return 'B'
-    return 'D'
-
-# normalizes distance between cells (nice add-on, not necessary)
-def normalize():
-    return
+    def getCell(self, r, x, y, z):
+        print('probability at',x,y,z,r,math.sqrt((self.d/2-x)**2 +(self.d/2-y)**2 +(self.d/2-z)**2), self.d/6, self.ps[x][y][z])
+        if r <= self.ps[x][y][z][0]:
+            return 'A'
+        elif r <= self.ps[x][y][z][1]:
+            return 'B'
+        return 'D'
 
 # how alpha cell in one position affects the probability a beta cell will be near it and so on
 mult = [[0.03,0.0,0.0],[0.0,0.03,0.0],[0.0,0.0,0.02]]
 # initialize distribution
-dist = Space(ct, n, sc, sys.argv[1])
+dist = Space(probabilities, sizes, n, sys.argv[1])
 dist.mainLoop_B()
 dist.plot()
 dist.writeData()
