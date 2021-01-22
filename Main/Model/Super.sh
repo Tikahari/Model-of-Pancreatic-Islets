c653393ae7e17e3da0e5f331c5221b17c05cdeca
Robert Claar <robert727@login2.ufhpc>
2021-01-22 10:01:06 -0500

    Added ml neuron into Super.sh

--- Main/Model/Super.sh
+++ Main/Model/Super.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-id="model_5x5x5_10000ms_0.15_0.60_0.25"
+id="model_5x5x5_10000ms_0.35_0.50_0.15"
 size="4"
 simulation_time=15000
-alpha_probability=0.15
-beta_probability=0.60
-delta_probability=0.25
+alpha_probability=0.35
+beta_probability=0.50
+delta_probability=0.15
 alpha_beta_combined=$(echo "$alpha_probability + $beta_probability" | bc)
 total=$(echo "$alpha_probability + $beta_probability + $delta_probability" | bc)
 if [[ $total != 1.00 ]]
@@ -16,6 +16,7 @@ fi
 echo -e "\nRunning model with:\nid=$id\nsize=$size\nsimulation_time=$simulation_time\nalpha_probability=$alpha_probability\nbeta_proability=$beta_probability\ndelta_probability=$delta_probability\n"
 read -n 1 -p "Verify with enter"
 touch log.txt
+ml neuron
 python3 Super.py $id $size 
 echo "------------ Super.py complete ----------"
 python3 Compile.py $id $size
