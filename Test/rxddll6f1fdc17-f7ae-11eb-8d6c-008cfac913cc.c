1b261f49fa2fe3114651877b4742a58c7e8b76a5
Claar727 <robert727@login2.ufhpc>
2021-08-08 12:16:10 -0400

    Added Insulin_test.py. Having a weird issue where subsequent simulations are different than the initial.

---
+++ Test/rxddll6f1fdc17-f7ae-11eb-8d6c-008cfac913cc.c
@@ -0,0 +1,13 @@
+#include <math.h>
+/*Some functions supported by numpy that aren't included in math.h
+ * names and arguments match the wrappers used in rxdmath.py
+ */
+double factorial(const double);
+double degrees(const double);
+void radians(const double, double*);
+double log1p(const double);
+double vtrap(const double, const double);
+void reaction(double* species_3d, double* params_3d, double* rhs)
+{
+	rhs[0] = Vector[29]-2000*(species_3d[0]);
+}
